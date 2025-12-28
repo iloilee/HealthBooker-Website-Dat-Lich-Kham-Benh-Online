eebdfa27a51bada7b91c88378e29a1a17b9c3695
iLoileDev <loiledelta0@gmail.com>
2025-12-28 14:39:12 +0700

    bo setting

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -142,13 +142,13 @@
           </a>       
         </nav>
         <div class="mt-auto p-4">
-          <a
+          {{-- <a
             class="flex items-center gap-3 rounded-lg px-3 py-2 text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 hover:text-slate-900 dark:hover:text-slate-50"
             href="{{ route('admin.settings') }}"
           >
             <span class="material-symbols-outlined !text-xl">settings</span>
             <span class="text-sm font-medium">Cài đặt</span>
-          </a>
+          </a> --}}
           <form method="POST" action="{{ route('logout') }}">
             @csrf
             <button 
